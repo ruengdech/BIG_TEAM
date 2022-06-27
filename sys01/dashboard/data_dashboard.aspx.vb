04c80d92133c29d96956c48c12b5d2ba7d6bd6cb
Kornnalin <32424977+kornnalin@users.noreply.github.com>
2022-06-27 14:30:34 +0700

    June 27 update dashboard/ AM , BM , Engineer Data
    Already done and test . Dashboard/ AM , BM , Engineer Data
    separate update 8 files.

--- sys01/dashboard/data_dashboard.aspx.vb
+++ sys01/dashboard/data_dashboard.aspx.vb
@@ -1,5 +1,22 @@
 ﻿
 Partial Class sys01_dashboard_New_DataTable
     Inherits System.Web.UI.Page
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        If Not (Me.IsPostBack) Then
+
+            ddl_year.Items.FindByValue(DateTime.Now.ToString("yyyy")).Selected = True
+
+        End If
+
+        'ddl_year.SelectedValue = DateTime.Now.ToString("yyyy")
+        Dim req_group, req_year As String
+        req_group = ddl_reqGroup.Text
+        req_year = ddl_year.Text
+        '*** Create Session ***'
+        Session.Timeout = 20
+        Session("Group") = req_group
+        Session("Year") = req_year
+    End Sub
 
 End Class
